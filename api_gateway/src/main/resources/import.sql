9e9ec4b1757d9259b9208e7201f7155a8afad571
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2023-10-12 14:10:09 -0300

    Implementaçao Kong e testes

--- api_gateway/src/main/resources/import.sql
+++ api_gateway/src/main/resources/import.sql
@@ -1,11 +1,11 @@
-INSERT INTO ordemservico(numero, descricao) VALUES (1, 'OS-Escritório');
-INSERT INTO ordemservico(numero, descricao) VALUES (2, 'OS-Informática');
-INSERT INTO ordemservico(numero, descricao) VALUES (3, 'OS-Fábrica');
-INSERT INTO ordemservico(numero, descricao) VALUES (4, 'OS-Estoque');
+INSERT INTO ordemservico(numero, descricao) VALUES (10, 'OS-Escritório');
+INSERT INTO ordemservico(numero, descricao) VALUES (20, 'OS-Informática');
+INSERT INTO ordemservico(numero, descricao) VALUES (30, 'OS-Fábrica');
+INSERT INTO ordemservico(numero, descricao) VALUES (40, 'OS-Estoque');
 
-INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (10, 'Imp1', 'Impressora', 'Sem tinta', 1);
-INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (11, 'Imp2', 'Impressora', 'Enrosca papel', 1);
-INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (12, 'Xerox1', 'Copiadora', 'Sem tonner', 2);
-INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (13, 'Scan1', 'Scanner', 'Nao liga', 3);
-INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (14, 'LC1', 'Leitor QR-Code', 'Quebrado', 4);
+INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (110, 'Imp1', 'Impressora', 'Sem tinta', 1);
+INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (120, 'Imp2', 'Impressora', 'Enrosca papel', 1);
+INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (130, 'Xerox1', 'Copiadora', 'Sem tonner', 2);
+INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (140, 'Scan1', 'Scanner', 'Nao liga', 3);
+INSERT INTO equipamento(numero, indice, descricao, problema, os_id) VALUES (150, 'LC1', 'Leitor QR-Code', 'Quebrado', 4);
 
